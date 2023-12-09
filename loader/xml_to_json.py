98722dc57c07835a20d322cbc7a84dba644d6d4b
peacedukej <lovecoldreams@gmail.com>
2023-12-10 00:28:38 +0300

    Added users.json and xml to json converter

--- loader/xml_to_json.py
+++ loader/xml_to_json.py
@@ -1,9 +1,27 @@
+# import xmltodict
+# import json
+
+# with open('loader/data_loader/Users.xml', 'r', encoding='utf-8') as xml_file:
+#     data_dict = xmltodict.parse(xml_file.read())
+#     json_data = json.dumps(data_dict, indent=2)
+
+# with open('Users.json', 'w') as json_file:
+#     json_file.write(json_data)
+
 import xmltodict
 import json
 
-with open('C:/Users/Rabinowitz/Desktop/nosql_db/mai-nosql-project/data/Users.xml') as xml_file:
+with open('loader/data_loader/Users.xml', 'r', encoding='utf-8') as xml_file:
     data_dict = xmltodict.parse(xml_file.read())
-    json_data = json.dumps(data_dict, indent=2)
 
-with open('Users.json', 'w') as json_file:
+# Извлечь только содержимое ключа 'DisplayName'
+display_names = [entry['@DisplayName'] for entry in data_dict['users']['row']]
+
+# Создать новый словарь с ключом 'displayName' и списком значений
+filtered_data = {'displayName': display_names}
+
+# Преобразовать в JSON
+json_data = json.dumps(filtered_data, indent=2, ensure_ascii=False)
+
+with open('Users.json', 'w', encoding='utf-8') as json_file:
     json_file.write(json_data)
