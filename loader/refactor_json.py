d755ff72448507402ab944703c731794aa18ee66
peacedukej <lovecoldreams@gmail.com>
2023-12-10 01:28:40 +0300

    Added mongoimport tool

---
+++ loader/refactor_json.py
@@ -0,0 +1,12 @@
+import json
+
+# Чтение имен из файла users.json
+with open("C:/Users/Rabinowitz/Desktop/nosql_db/mai-nosql-project/loader/data_loader/Users.json", "r", encoding="utf-8") as input_file:
+    names = [((line.replace(',', '')).replace('"', '')).strip() for line in input_file]
+
+# Создание списка словарей для каждого имени
+data = [{"name": name} for name in names]
+
+# Запись данных в новый JSON файл
+with open("UsersNew.json", "w", encoding="utf-8") as json_file:
+    json.dump(data, json_file, ensure_ascii=False, indent=2)
