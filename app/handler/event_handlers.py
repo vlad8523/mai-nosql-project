bd7290c062f7d02b0d1db790c2c217b097421ebb
EugIva <145147798+EugIva@users.noreply.github.com>
2023-12-17 16:58:46 +0300

    update elastic, etc
    update elasticsearch implementation, startup and shutdown, .env

--- app/handler/event_handlers.py
+++ app/handler/event_handlers.py
@@ -2,10 +2,15 @@ import asyncio
 
 from utils.mongo_utils import connect_and_init_mongo, close_mongo_connect
 
+from utils.elasticsearch_utils import connect_and_init_elasticsearch, close_elasticsearch_connect
+
 
 async def startup():
-    connect_and_init_mongo()
+    init_mongo_future = connect_and_init_mongo()
+    init_elasticsearch_future = connect_and_init_elasticsearch()
+    await asyncio.gather(init_mongo_future, init_elasticsearch_future)
 
 
 async def shutdown():
     close_mongo_connect()
+    await close_elasticsearch_connect()
--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 
+from handler.event_handlers import startup, shutdown
 from router.bookings_router import booking_router
 from router.clients_router import client_router
 from router.rooms_router import room_router
 from utils.mongo_utils import connect_and_init_mongo, close_mongo_connect
+
 
 app = FastAPI()
 
 app.include_router(client_router, tags=['Client'], prefix='/api/clients')
 app.include_router(room_router, tags=['Room'], prefix='/api/rooms')
 app.include_router(booking_router, tags=['Booking'], prefix='/api/bookings')
-app.add_event_handler("startup", connect_and_init_mongo)
-app.add_event_handler("shutdown", close_mongo_connect)
+app.add_event_handler("startup", startup)
+app.add_event_handler("shutdown", shutdown)
