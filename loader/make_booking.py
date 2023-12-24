f7353e939fe964b96435df053d4e78b42a13f348
peacedukej <lovecoldreams@gmail.com>
2023-12-24 15:27:47 +0300

    Added uvicorn

--- loader/make_booking.py
+++ loader/make_booking.py
@@ -2,7 +2,7 @@ import pymongo
 from bson import ObjectId
 from random import choice
 from datetime import datetime, timedelta
-
+import json, random, time
 # Подключение к MongoDB
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 database = client["booking-db"]
@@ -16,22 +16,32 @@ booking_collection = database["bookings"]
 clients = list(clients_collection.find())
 rooms = list(rooms_collection.find())
 
+array = []
+current_datetime = datetime.today() - timedelta(days=random.randint(1, 10))
+next_day = current_datetime + timedelta(days=random.randint(1, 10))
 # Генерация записей в таблице booking
-for i in range(10000):  # Например, создадим 10 записей
+for i in range(10):  # Например, создадим 10 записей
     client_id = choice(clients)["_id"]
     room_id = choice(rooms)["_id"]
-    booking_dates = [
-        (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d"),  # Дата начала брони (на завтра)
-        (datetime.now() + timedelta(days=2)).strftime("%Y-%m-%d")   # Дата окончания брони (послезавтра)
-    ]
-    booking_status = choice(["0", "1"])
+    #booking_dates = [
+    #    (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d"),  # Дата начала брони (на завтра)
+    #    (datetime.now() + timedelta(days=2)).strftime("%Y-%m-%d")   # Дата окончания брони (послезавтра)
+    #]
+    booking_start = int(time.mktime(current_datetime.timetuple()))
+    booking_end = int(time.mktime(next_day.timetuple()))
+    booking_status = choice(["booked", "paid", "free"])
 
     # Вставка записи в таблицу booking
-    booking_collection.insert_one({
+    array.append({
         "client_id": client_id,
         "room_id": room_id,
-        "booking_dates": booking_dates,
+        "booking_start": booking_start,
+        "booking_end": booking_end,
         "booking_status": booking_status
     })
+#booking_collection.insert_many(array)
+with open("bookings.json", "w") as file:
+    file.write(str(array))
+    #json.dump(array, file)
 
 print("Записи успешно добавлены в таблицу booking.")
